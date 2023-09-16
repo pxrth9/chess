b311d53d1c2f7c1d6c1c49e88ca74856080f0242
Parth <76231594+pxrth9@users.noreply.github.com>
2023-09-15 21:02:03 -0400

    added all the final changes

--- download_games.sh
+++ download_games.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
 BASE_URL="https://api.chess.com/pub/player/"
+PLAYER_USERNAME=$1
 GAMES="/games/"
-MONTH=$1
-echo "$MONTH"
-MONTH=$(printf "%02d" "$1")
-echo "$MONTH"
-YEAR=$2
-echo "$YEAR"
-echo "$0 $1 $2"
-PLAYER_USERNAME="pparth86"
+MONTH=$(printf "%02d" "$2")
+YEAR=$3
 
 # Create the Games folder if it doesn't exist
 mkdir -p "Games/$YEAR/$MONTH"
 
 REQUEST_URL="$BASE_URL$PLAYER_USERNAME$GAMES$YEAR/$MONTH"
-echo "$REQUEST_URL"
 RESPONSE=$(curl -s "$REQUEST_URL")
-echo "$RESPONSE"
 
 # get the length of the games array
 GAMES_LENGTH=$(echo "$RESPONSE" | jq '.games | length')
@@ -25,6 +18,7 @@ echo "Number of Games: $GAMES_LENGTH "
 
 # Loop through all games in the response
 for ((i = 0; i < GAMES_LENGTH; i++)); do
+    echo "Processing Game #: $i"
     PGN=$(echo "$RESPONSE" | jq -r ".games[$i].pgn")
     echo "$PGN" > "Games/$YEAR/$MONTH/$i.pgn"
 done
