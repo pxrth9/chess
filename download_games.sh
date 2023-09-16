499dc057310eb7fe6ea46096e9eb70b1227964ff
Parth <76231594+pxrth9@users.noreply.github.com>
2023-09-15 20:14:55 -0400

    added workflows

---
+++ download_games.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+BASE_URL="https://api.chess.com/pub/player/"
+GAMES="/games/"
+MONTH=$1
+YEAR=$2
+PLAYER_USERNAME="pparth86"
+
+# Create the Games folder if it doesn't exist
+mkdir -p "Games/$YEAR/$MONTH"
+
+VAR=1
+REQUEST_URL="$BASE_URL$PLAYER_USERNAME$GAMES$YEAR/$MONTH"
+RESPONSE=$(curl -s "$REQUEST_URL")
+
+# get the length of the games array
+GAMES_LENGTH=$(echo "$RESPONSE" | jq '.games | length')
+echo "Number of Games: $GAMES_LENGTH "
+
+# Loop through all games in the response
+for ((i = 0; i < GAMES_LENGTH; i++)); do
+    PGN=$(echo "$RESPONSE" | jq -r ".games[$i].pgn")
+    echo "$PGN" > "Games/$YEAR/$MONTH/$VAR.pgn"
+    VAR=$((VAR+1))
+done
+
+echo "Done!"
