0f431c05f8cf726e9e4feb703fdc20b8783f9ebb
Parth Patel <ppatil91099@gmail.com>
2025-03-09 22:19:09 -0400

    finalized changes

--- lichess.py
+++ lichess.py
@@ -8,7 +8,7 @@ LICHESS_TOKEN = os.environ.get("LICHESS_TOKEN")
 def download_games_lichess(username, since, until):
     session = berserk.TokenSession(LICHESS_TOKEN)
     client = berserk.Client(session=session)
-    start, end = berserk.utils.to_millis(start), berserk.utils.to_millis(end)
+    start, end = berserk.utils.to_millis(since), berserk.utils.to_millis(until)
     games_resp = client.games.export_by_player(
         username=username,
         since=start,
