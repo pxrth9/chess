e69c4deab7f9d6f4b48cc28935792c3dcf19a5fe
Parth Patel <ppatil91099@gmail.com>
2025-04-04 23:09:26 -0400

    structured directory

---
+++ src/utils/logger.py
@@ -0,0 +1,14 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Define the log format
+)
+
+logger = logging.getLogger("musync")  # Use a custom name for the logger
+
+
+def set_debug_mode():
+    logger.setLevel(logging.DEBUG)
+    logging.getLogger().setLevel(logging.DEBUG)  # Update the root logger level
+    logger.debug("Logging level set to DEBUG")
